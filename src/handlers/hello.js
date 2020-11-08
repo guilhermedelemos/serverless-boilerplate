c967e0a6326436c2ece55491cbd21ce6501feb7a
Guilherme de Lemos <guilherme.eti@gmail.com>
2020-11-08 20:23:28 -0300

    build(lambda): add webpack

--- src/handlers/hello.js
+++ src/handlers/hello.js
@@ -1,8 +1,6 @@
-/* eslint-disable import/prefer-default-export */
-
 import { v4 as uuidv4 } from 'uuid';
 
-export const handler = async (event, context) => {
+const handler = async (event, context) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -13,7 +11,6 @@ export const handler = async (event, context) => {
     }),
   };
   return response;
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+export default handler;
